24d206cbd9a023e19d9f083ca2108cef48c1c39b
jiajun-luo <luo.jiajun.jiajun@gmail.com>
2024-01-07 14:51:24 -0500

    Add files via upload

---
+++ Disable-IdlePowerSaving.ps1
@@ -0,0 +1,4 @@
+﻿#Disable the Idle Power Saving setting on Realtek USB GbE Family Controller network adapter 
+Get-NetAdapter | Where InterfaceDescription -like "Realtek USB GbE Family Controller*" | % {  
+	Set-NetAdapterAdvancedProperty -Name $_.Name -DisplayName "Idle Power Saving" -DisplayValue "Disabled"	  
+}
